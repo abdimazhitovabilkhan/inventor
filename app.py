a2b9fdc387736f0ecfaf2b0b46033464c40619da
abdimazhitovabilkhan <abdimazhitivabilkhan@gmail.com>
2021-07-14 21:10:51 +0600

    added new functions

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#from werkzeug.wrappers import request
 app = Flask(__name__)
 @app.route("/")
 def homepage():
-    f = open("goods.xlsx", "a", encoding="utf-8")
-    xlsx = f.readlines()
-    return render_template("index.html", goods=xlsx)
+    f = open("goods.txt", "r", encoding="utf-8")
+    txt = f.readlines()
+    return render_template("index.html", goods=txt)
+
+@app.route("/add/", methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open("goods.txt", "a", encoding="utf-8")
+    f.write(good + "\n")
+    f.close()
+    return """
+    <h1>Запись была добавлена</h1>
+    <a href="/">Домой</a>
+    
+    """
